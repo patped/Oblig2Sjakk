dacdac555910fa941e7699eb80b0bd01844c8613
Patrick Pedersen <patrick@ossia.net>
2017-10-25 20:31:47 +0200

    laget eget posisjon objekt

---
+++ src/oblig2sjakk/Posisjon.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oblig2sjakk;
+
+/**
+ *
+ * @author patrick
+ */
+public class Posisjon {
+  
+  private int rad;
+  private int kolonne;
+
+  public Posisjon(String posisjon) {
+    rad = (posisjon.charAt(1) - 48) * -1 + 8;
+    kolonne = (posisjon.charAt(1) - 97);
+  }
+  
+}
