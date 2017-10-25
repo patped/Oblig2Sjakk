de8fe4e14a5950574ea0af4853ce1735b4648a24
Patrick Pedersen <patrick@ossia.net>
2017-10-25 20:13:28 +0200

    Fjernet erLovligTrekk()

--- src/oblig2sjakk/Tårn.java
+++ src/oblig2sjakk/Tårn.java
@@ -23,67 +23,6 @@ public class Tårn extends Brikke {
 
   @Override
   public boolean erLovligTrekk(String tilPosisjon) {
-    System.out.println("Nå: Er lovlig trekk");
-    boolean sjekker = true;
-    int valgtBokstav = this.getPosisjon().charAt(0) - 97;
-    System.out.println("valgtBokstav: " + valgtBokstav);
-    int valgtTall = this.getPosisjon().charAt(1) - 49;
-    System.out.println("valgtTall: " + valgtTall);
-    int tilBokstav = tilPosisjon.charAt(0) - 97;
-    System.out.println("tilBokstav: " + tilBokstav);
-    int tilTall = (tilPosisjon.charAt(1) - 48) * -1 + 8;
-    System.out.println("tilTall: " + tilTall);
-
-    if (valgtBokstav == tilBokstav // Sjekker om den løper på samme bokstavrekke og at tallet er ulikt
-        && valgtTall != tilTall) {
-      if (valgtTall - tilTall < 0) {//Sjekker om tårnet kan løpe mellom de to plassene.
-        for (int i = -1; i > valgtTall - tilTall; i--) {// Når den løper oppover på brettet.
-          if (brett.brikker[valgtBokstav][valgtTall] != null) {
-            sjekker = false;
-          }
-        }
-      } else {
-        for (int i = 1; i < valgtTall - tilTall; i++) {//Når den løper nedover på brettet.
-          if (brett.brikker[valgtBokstav][valgtTall] != null) {
-            sjekker = false;
-          }
-        }
-      }
-      if (sjekker == true && brett.brikker[tilBokstav][tilTall] == null) {
-        return true;
-      }
-      System.out.println("Tester 1: " + (sjekker == true
-          && brett.brikker[tilBokstav][tilTall].isFarge() != this.isFarge()));
-      return (sjekker == true
-          && brett.brikker[tilBokstav][tilTall].isFarge() != this.isFarge());
-
-    }
-    if (valgtBokstav != tilBokstav // Sjekker om den løper på samme tall og at bokstaven er ulikt
-        && valgtTall == tilTall) {
-      System.out.println("Går jeg inn i IF-testen Bokstav ulik bokstav + tall er lik tall");
-      if (valgtBokstav - tilBokstav < 0) {//Sjekker om tårnet kan løpe mellom de to plassene.
-        for (int i = -1; i > valgtBokstav - tilBokstav; i--) {// Når den løper mot høyre på brettet.
-          if (brett.brikker[valgtBokstav - i][valgtTall] != null) {
-            sjekker = false;
-          }
-        }
-      } else {
-        for (int i = 1; i < valgtBokstav - tilBokstav; i++) {// Når den løper oppover på brettet.
-          if (brett.brikker[valgtBokstav - i][valgtTall] != null) {
-            sjekker = false;
-          }
-        }
-      }
-      if (sjekker == true && brett.brikker[tilBokstav][tilTall] == null) {
-        return true;
-      }
-      System.out.println("Nr 2: " + (sjekker == true
-          && brett.brikker[tilBokstav][tilTall].isFarge() != this.isFarge()));
-      return (sjekker == true
-          && brett.brikker[tilBokstav][tilTall].isFarge() != this.isFarge());
-    }
-    System.out.println("Test 3: " + (valgtBokstav != tilBokstav // Sjekker om den løper på samme tall og at bokstaven er ulikt
-        && valgtTall == tilTall));
-    return false;
+    
   }
 }
