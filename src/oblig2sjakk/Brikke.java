02afe17a189b5c3f1dbf2ef2de34a2b8a8336fdd
Patrick Pedersen <patrick@ossia.net>
2017-10-25 20:35:46 +0200

    gjort om Brikke til å bruke Posisjon

--- src/oblig2sjakk/Brikke.java
+++ src/oblig2sjakk/Brikke.java
@@ -5,32 +5,18 @@ import javafx.scene.shape.Rectangle;
 
 public abstract class Brikke extends Rectangle {
 
-  private int rad;
-  private int kolonne;
+  private Posisjon posisjon;
   private boolean farge;
   protected Brett brett;
   final double lendge = 55;
 
   public Brikke(String posisjon, boolean farge, Brett brett) {
-    setPosisjon(posisjon);
+    this.posisjon = new Posisjon(posisjon);
     this.farge = farge;
     this.setHeight(lendge);
     this.setWidth(lendge);
     this.setFill(Color.TRANSPARENT);
     this.brett = brett;
-  }
-
-  public int getRad() {
-    return rad;
-  }
-
-  public int getKolonne() {
-    return kolonne;
-  }
-
-  private void setPosisjon(String rutenavn) {
-    rad = (rutenavn.charAt(1) - 48) * -1 + 8;
-    kolonne = (rutenavn.charAt(1) - 97);
   }
 
   public boolean isFarge() {
@@ -41,8 +27,7 @@ public abstract class Brikke extends Rectangle {
 
   public boolean flyttTil(String rutenavn) {
     if (this.erLovligTrekk(rutenavn)) {
-      System.out.println("er erLovlig trekk true? ");
-      setPosisjon(rutenavn);
+      posisjon = new Posisjon(rutenavn);
       return true;
     }
     return false;
