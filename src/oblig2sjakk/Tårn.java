51022ef3048a374dac9d558254b39f8c87efbcc1
Patrick Pedersen <patrick@ossia.net>
2017-10-25 21:01:58 +0200

    Begynt på erLovligTrekk()

--- src/oblig2sjakk/Tårn.java
+++ src/oblig2sjakk/Tårn.java
@@ -23,6 +23,30 @@ public class Tårn extends Brikke {
 
   @Override
   public boolean erLovligTrekk(String tilPosisjon) {
+    Posisjon nyPosisjon = new Posisjon(tilPosisjon);
+    int x = posisjon.getKolonne();
+    int y = posisjon.getRad();
+    int x2 = nyPosisjon.getKolonne();
+    int y2 = nyPosisjon.getRad();
     
+    if (x2<x) {
+      int tmp = x;
+      x = x2;
+      x2 = tmp;
+    }
+    
+    if (y2<y) {
+      int tmp = y;
+      y = y2;
+      y2 = tmp;
+    }
+    
+    double a = (y2 - y) / (x2 - x);
+    double b = y - a * x;
+    
+    for ()
+    return p.y == a * p.x + b
+        && p.x + p.y > x + y
+        && p.x + p.y < x2 + y2;
   }
 }
