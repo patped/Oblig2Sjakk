3c0f0568803a0504baf4b4317067a4d117d71c6e
Patrick Pedersen <patrick@ossia.net>
2017-10-26 01:32:04 +0200

    Laget ny konstruktør for bonde og konge

--- src/oblig2sjakk/Bonde.java
+++ src/oblig2sjakk/Bonde.java
@@ -1,30 +1,37 @@
-
 package oblig2sjakk;
 
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 
 public class Bonde extends Brikke {
-    
-    private final String BOKSTAV = "b";
 
-    public Bonde(String posisjon, boolean farge, Brett brett) {
-        super(posisjon, farge, brett);
-        if (farge == false) {
-           this.setFill(new ImagePattern(new Image("/svartbonde.png")));
-        }
-        else{
-            this.setFill(new ImagePattern(new Image("/hvitbonde.png")));
-        }
-    }
+  private final String BOKSTAV = "b";
 
-    @Override
-    public String brikkenavn() {
-        return BOKSTAV;
+  public Bonde(String posisjon, boolean farge, Brett brett) {
+    super(posisjon, farge, brett);
+    if (farge == false) {
+      this.setFill(new ImagePattern(new Image("/svartbonde.png")));
+    } else {
+      this.setFill(new ImagePattern(new Image("/hvitbonde.png")));
     }
+  }
 
-    @Override
-    public boolean erLovligTrekk(String tilPosisjon) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public Bonde(int kolonne, int rad, boolean farge, Brett brett) {
+    super(kolonne, rad, farge, brett);
+    if (farge == false) {
+      this.setFill(new ImagePattern(new Image("/svartbonde.png")));
+    } else {
+      this.setFill(new ImagePattern(new Image("/hvitbonde.png")));
     }
+  }
+
+  @Override
+  public String brikkenavn() {
+    return BOKSTAV;
+  }
+
+  @Override
+  public boolean erLovligTrekk(String tilPosisjon) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
 }
--- src/oblig2sjakk/Konge.java
+++ src/oblig2sjakk/Konge.java
@@ -1,37 +1,43 @@
-
 package oblig2sjakk;
 
 import javafx.scene.image.Image;
 import javafx.scene.paint.ImagePattern;
 
-
 public class Konge extends Brikke {
-    
-    private final String BOKSTAV = "k";
 
-    public Konge(String posisjon, boolean farge, Brett brett) {
-        super(posisjon, farge, brett);
-                if (farge == false) {
-           this.setFill(new ImagePattern(new Image("/svartkonge.png")));
-        }
-        else{
-            this.setFill(new ImagePattern(new Image("/hvitkonge.png")));
-        }
-    }
+  private final String BOKSTAV = "k";
 
-    @Override
-    public String brikkenavn() {
-       return BOKSTAV;
+  public Konge(String posisjon, boolean farge, Brett brett) {
+    super(posisjon, farge, brett);
+    if (farge == false) {
+      this.setFill(new ImagePattern(new Image("/svartkonge.png")));
+    } else {
+      this.setFill(new ImagePattern(new Image("/hvitkonge.png")));
     }
+  }
 
-    @Override
-    public boolean flyttTil(String rutenavn) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public Konge(int kolonne, int rad, boolean farge, Brett brett) {
+    super(kolonne, rad, farge, brett);
+    if (farge == false) {
+      this.setFill(new ImagePattern(new Image("/svartkonge.png")));
+    } else {
+      this.setFill(new ImagePattern(new Image("/hvitkonge.png")));
     }
+  }
 
-    @Override
-    public boolean erLovligTrekk(String tilPosisjon) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+  @Override
+  public String brikkenavn() {
+    return BOKSTAV;
+  }
+
+  @Override
+  public boolean flyttTil(String rutenavn) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
+
+  @Override
+  public boolean erLovligTrekk(String tilPosisjon) {
+    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  }
 
 }
